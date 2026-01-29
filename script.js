180108dbd2b767f6b2797200808bb7e0e4388300
Gabriel <gabrielalveslga1155@gmail.com>
2026-01-29 11:55:29 -0300

    update to script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const btn = document.querySelector('button')
 const aside = document.querySelector('aside.menuBar')
 
 btn.addEventListener('click', () => {
+    btn.innerHTML = '<i class="fa-solid fa-bars"></i>'
     if (window.getComputedStyle(aside).display == 'flex'){
-        btn.innerHTML = '<i class="fa-solid fa-bars"></i>'
         aside.style.display = 'none'
     } else{
-        btn.innerHTML = '<i class="fa-solid fa-x"></i>'
+        btn.innerHTML = '<i class="fa-solid fa-x"></i> '
         aside.style.display = 'flex'
     }
 
